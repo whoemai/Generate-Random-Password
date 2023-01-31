8d3ef33bb2aa7f2af738d0a7c6d2c8d3ba36f28e
Pedro Fernandes <pedro.fernandes.moura@gmail.com>
2023-01-31 13:09:05 -0300

    adding copy password button

--- appy.py
+++ appy.py
@@ -1,29 +1,30 @@
+import tkinter as tk
 import random
 import string
-import tkinter as tk
-from tkinter import Entry, Label, Button
-
+import pyperclip
 
 def generate_password():
-    characters = "-" + string.ascii_letters + "-" + "@" + string.digits + "-"
-    length = entry.get()
-    password = ''.join(random.choice(characters) for i in range(int(length)))
-    label.config(text=f"Your password gereta: {password}")
+    password = ''.join(random.sample(string.ascii_letters + string.digits, 20))
+    label["text"] = password
 
+def copy_password():
+    pyperclip.copy(label["text"])
 
 root = tk.Tk()
 root.title("Password Generator")
-root.geometry("500x400+100+100")
+root.geometry("400x150")
 
-label = Label(root, text="")
-label.pack(side="top", fill="both", expand=True, anchor="center")
+label = tk.Label(root, text="")
+label.pack(side="top", fill="both", expand=True)
 
-entry = Entry(root, width=50)
-entry.pack(side="top", fill="both", expand=True, anchor="center")
+entry = tk.Entry(root, width=50)
+entry.pack(side="top", fill="both", expand=True)
 entry.insert(0, 20)
 
-button = Button(root, text="Generate", command=generate_password)
-button.pack(side="bottom", fill="both", expand=True, anchor="center", pady=25, padx=25)
+generate_button = tk.Button(root, text="Generate", command=generate_password)
+generate_button.pack(side="left", padx=10)
+
+copy_button = tk.Button(root, text="Copy", command=copy_password)
+copy_button.pack(side="right", padx=10)
 
 root.mainloop()
-
