558774eb0153d25288d8f1e20f6556049a365365
Pedro Fernandes <pedro.fernandes.moura@gmail.com>
2023-01-31 12:36:01 -0300

    putting graphical interface

--- appy.py
+++ appy.py
@@ -1,11 +1,27 @@
 import random
 import string
+import tkinter as tk
+from tkinter import Entry, Label, Button
 
 
-def generate_password(length=20):
+def generate_password():
     characters = "-" + string.ascii_letters + "-" + "@" + string.digits + "-"
-    password = ''.join(random.choice(characters) for i in range(length))
-    return password
+    length = entry.get()
+    password = ''.join(random.choice(characters) for i in range(int(length)))
+    label.config(text=f"Your password gereta: {password}")
 
 
-print(f"Your password gereta: {generate_password()}")
+root = tk.Tk()
+root.title("Password Generator")
+
+label = Label(root, text="")
+label.pack()
+
+entry = Entry(root, width=50)
+entry.pack()
+entry.insert(0, 20)
+
+button = Button(root, text="Generate", command=generate_password)
+button.pack()
+
+root.mainloop()
